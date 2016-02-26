182bb8788bc7f4eb3970fb3aff5eaa3a8d4f776a
Kurt Stephens <ks.github@kurtstephens.com>
2016-02-26 16:16:57 -0600

    Fix ex01 /.

--- example/ex01/app.rb
+++ example/ex01/app.rb
@@ -4,10 +4,16 @@ require 'haml'
 gem 'awesome_print'; require 'ap'
 
 class App < Sinatra::Application
+  include RackConsole::Configuration
   set :views, "#{File.expand_path('..', __FILE__)}/template/haml"
-
   get '/?' do
     haml :'index'
+  end
+
+  helpers do
+    def config
+      { }
+    end
   end
 end
 
