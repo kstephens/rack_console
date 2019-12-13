b83315f7805de4e1a38927d4297b42746d277665
Kurt Stephens <ks.github@kurtstephens.com>
2019-12-13 13:29:08 -0600

    Clean up.

--- lib/pp_annotated.rb
+++ lib/pp_annotated.rb
@@ -15,11 +15,7 @@ class PPAnnotated < PP
   # cannot call super.
   def self.pp_internal(annotater, obj, out=$>, width=79)
     q = new(annotater, out, width)
-    # binding.pry
-    q.guard_inspect_key do
-      # binding.pry
-      q.pp obj
-    end
+    q.guard_inspect_key {q.pp obj}
     q.flush
     #$pp = q
     out << "\n"
@@ -32,31 +28,12 @@ class PPAnnotated < PP
 
   module PPMethods
     include ::PP::PPMethods
-    # define_method(:pp, PP::PPMethods.instance_method(:pp))
     alias :pp_super :pp
     def pp *args
-      # binding.pry
       @annotater.pp(self, *args) do
         pp_super(*args)
       end
     end
-=begin
-  alias :pretty_print_super :pretty_print
-  def pretty_print *args
-    binding.pry
-    @annotater.pretty_print(self, *args) do
-      pretty_print_super(*args)
-    end
-  end
-
-  alias :text_super :text
-  def text(obj, width=obj.length)
-    binding.pry
-    @annotater.text(self, obj, width) do
-      text_super(obj, width)
-    end
-  end
-=end
   end
   prepend PPMethods
 
@@ -64,26 +41,7 @@ class PPAnnotated < PP
     def initialize annotater, *args
       @annotater = annotater
       super(*args)
-      # binding.pry
     end
-
-=begin
-  alias :pretty_print_super :pretty_print
-  def pretty_print *args
-    binding.pry
-    @annotater.pretty_print(self, *args) do
-      pretty_print_super(*args)
-    end
-  end
-
-  alias :text_super :text
-  def text(obj, width=obj.length)
-    binding.pry
-    @annotater.text(self, obj, width) do
-      text_super(obj, width)
-    end
-  end
-=end
   end
 
   # Base class
