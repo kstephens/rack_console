176fbf288ecf49702a6a94ec61b8224f115f5044
Kurt Stephens <git@kurtstephens.com>
2023-01-21 12:00:21 -0600

    More examples.

--- spec/lib/rack_console/ansi2html_spec.rb
+++ spec/lib/rack_console/ansi2html_spec.rb
@@ -48,7 +48,6 @@ module RackConsole
             str = "BEFORE-#{str}-AFTER"
             html = "BEFORE-#{html}-AFTER"
           end
-          # html = "<div class=\"ansi\">#{html}</div>"
           actual = subject.convert(str)
           if ENV['TEST_VERBOSE']
             puts "\n  ### escaped:   #{str.inspect} =>"
@@ -64,22 +63,8 @@ module RackConsole
             </table>
             <hr/>
           HTML
-          # expect(actual) .to eq html
+          expect(actual) .to eq html
         end
-      end
-
-      context "blink" do
-        _ "\e[5;m(BLINK-SLOW)\e[0m",
-          "<span class=\"ansi-5\">(BLINK-SLOW)</span>"
-        _ "\e[6;31;100;m(BLINK-RAPID)\e[0m",
-          "<span class=\"ansi-6 ansi-31 ansi-100\">(BLINK-RAPID)</span>"
-      end
-
-      context "ansi-51" do
-        _ "\e[51;36m(FRAMED)\e[0m",
-        "<span class=\"ansi-51 ansi-36\">(FRAMED)</span>"
-          _ "\e[52;36m(ENCIRCLED)\e[0m",
-          "<span class=\"ansi-52 ansi-36\">(ENCIRCLED)</span>"
       end
 
       context "basic text" do
@@ -103,29 +88,61 @@ module RackConsole
       end
 
       context "no reset" do
-        _ "BEFORE-\e[1m(BOLD)\e[3m(ITALIC)\e[22m-AFTER",
-        "BEFORE-<span class=\"ansi-1\">(BOLD)<span class=\"ansi-3\">(ITALIC)<span class=\"ansi-22\">-AFTER</span></span></span>",
-        false
+        _ "BEFORE-\e[1m(BOLD)\e[3m(AND-ITALIC)\e[22m-AFTER",
+          "BEFORE-<span class=\"ansi-1\">(BOLD)<span class=\"ansi-3\">(AND-ITALIC)<span class=\"ansi-22\">-AFTER</span></span></span>",
+          false
+        _ "\e[1;m(BOLD)",
+          "<span class=\"ansi-1\">(BOLD)</span>",
+           false
       end
 
       context "text styles" do
-        _ "\e[1;m(BOLD)",
-          "<span class=\"ansi-1\">(BOLD)</span>", false
         _ "\e[2;m(FAINT)\e[0m",
           "<span class=\"ansi-2\">(FAINT)</span>"
         _ "\e[3;m(ITALIC)\e[0m",
           "<span class=\"ansi-3\">(ITALIC)</span>"
-        _ "\e[1;m(BOLD)\e[3;m(ITALIC)\e[0m",
-        "<span class=\"ansi-1\">(BOLD)<span class=\"ansi-3\">(ITALIC)</span></span>"
-        _ "\e[31;m(red_foreground)\e[0m",
-          "<span class=\"red_foreground\">(red_foreground)</span>"
-        _ "\e[1;32;m(ansi-1-ansi-32)\e[0m",
-          "<span class=\"ansi-1 ansi-32\">(ansi-1-ansi-32)</span>"
-        _ "\e[33;45;m(ansi-33-ansi-45)\e[0m",
-          "<span class=\"ansi-33 ansi-45\">(ansi-33-ansi-45)</span>"
+        _ "\e[1;m(BOLD)\e[3;m(AND-ITALIC)\e[0m",
+          "<span class=\"ansi-1\">(BOLD)<span class=\"ansi-3\">(AND-ITALIC)</span></span>"
       end
 
-      context "8-bit color codes" do
+      context "framed" do
+        _ "\e[51;36m(FRAMED)\e[0m",
+        "<span class=\"ansi-51 ansi-36\">(FRAMED)</span>"
+          _ "\e[52;36m(ENCIRCLED)\e[0m",
+          "<span class=\"ansi-52 ansi-36\">(ENCIRCLED)</span>"
+      end
+
+      context "superscript/subscript" do
+        _ "\e[73m(SUPERSCRIPT)\e[0m",
+          "<span class=\"ansi-73\">(SUPERSCRIPT)</span>"
+        _ "\e[74m(SUBSCRIPT)\e[0m",
+        "<span class=\"ansi-74\">(SUBSCRIPT)</span>"
+      end
+
+      context "blink" do
+        _ "\e[5;m(BLINK-SLOW)\e[0m",
+          "<span class=\"ansi-5\">(BLINK-SLOW)</span>"
+        _ "\e[6;31;100;m(BLINK-RAPID)\e[0m",
+          "<span class=\"ansi-6 ansi-31 ansi-100\">(BLINK-RAPID)</span>"
+      end
+
+      context "basic color" do
+          "<span class=\"ansi-1\">(BOLD)<span class=\"ansi-3\">(AND-ITALIC)</span></span>"
+        _ "\e[31;m(RED)\e[0m",
+          "<span class=\"ansi-31\">(RED)</span>"
+        _ "\e[1;3;32;m(BOLD-ITALIC-GREEN)\e[0m",
+          "<span class=\"ansi-1 ansi-3 ansi-32\">(BOLD-ITALIC-GREEN)</span>"
+        _ "\e[1;34;45;m(BOLD-BLUE-ON-BRIGHT-MAGENTA)\e[0m",
+          "<span class=\"ansi-1 ansi-34 ansi-45\">(BOLD-BLUE-ON-BRIGHT-MAGENTA)</span>"
+      end
+
+      context "background colors" do
+        _ "\e[44;91;m(RED-BRIGHT-ON-BLUE)\e[0m",
+          "<span class=\"ansi-44 ansi-91\">(RED-BRIGHT-ON-BLUE)</span>"
+      end
+
+
+      context "8-bit color" do
         _ "\e[38;5;0m(000000)\e[0m",
         "<span class=\"ansi-38\" style=\"color: #000000;\">(000000)</span>"
         _ "\e[38;5;0;48;5;7m(white-on-black)\e[0m",
@@ -149,7 +166,7 @@ module RackConsole
         "<span class=\"ansi-38\" style=\"color: #eeeeee;\">(text)</span>"
       end
 
-      context "24-bit color codes" do
+      context "24-bit color" do
         _ "\e[38;2;16;200;255m(text)\e[0m",
         "<span class=\"ansi-38\" style=\"color: #10c8ff;\">(text)</span>"
       end
@@ -157,11 +174,6 @@ module RackConsole
       context "24-bit color codes and styles" do
         _ "\e[38;2;190;200;85;1;3m(COLOR-BOLD-ITALIC)\e[0m",
         "<span class=\"ansi-38 ansi-1 ansi-3\" style=\"color: #bec855;\">(COLOR-BOLD-ITALIC)</span>"
-      end
-
-      context "background colors" do
-        _ "\e[91;44;m(RED-BRIGHT-ON-BLUE)\e[0m",
-          "<span class=\"ansi-91 ansi-44\">(RED-BRIGHT-ON-BLUE)</span>"
       end
 
     end
